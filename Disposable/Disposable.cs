897c7133511d930737c52ce3ca033d8d99347fc0
Tech J Image <techjmage@outlook.com>
2025-02-28 09:25:38 +0000

    Add project files.

---
+++ Disposable/Disposable.cs
@@ -0,0 +1,28 @@
+﻿namespace Utility;
+
+public abstract class Disposable : IDisposable
+{
+    protected bool disposed = false;
+
+    /// <summary>
+    /// <seealso href="https://learn.microsoft.com/en-us/dotnet/standard/garbage-collection/implementing-dispose"/>
+    /// </summary>
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposed)
+            return;
+        if (disposing)
+            ReleaseResources();
+        disposed = true;
+    }
+    /// <summary>
+    /// Releases the resources. Override this method to releasing resources.
+    /// </summary>
+    public abstract void ReleaseResources();
+}
---
+++ Disposable/DisposableAsync.cs
@@ -0,0 +1,46 @@
+﻿namespace Utility;
+/// <summary>
+/// Async version of Disposable
+/// </summary>
+/// <seealso cref="System.IAsyncDisposable" />
+/// <seealso cref="System.IDisposable" />
+public class DisposableAsync : IAsyncDisposable, IDisposable
+{
+    protected bool disposed = false;
+
+    protected virtual async ValueTask DisposeAsync(bool disposing)
+    {
+        if (disposed)
+            return;
+        if (disposing)
+            await ReleaseResourcesAsync();
+        disposed = true;
+    }
+
+    public virtual ValueTask ReleaseResourcesAsync() => ValueTask.CompletedTask;
+
+    public async ValueTask DisposeAsync()
+    {
+        await DisposeAsync(true);
+        GC.SuppressFinalize(this);
+    }
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposed)
+            return;
+        if (disposing)
+            ReleaseResources();
+        disposed = true;
+    }
+
+    public virtual void ReleaseResources()
+    {
+
+    }
+}
